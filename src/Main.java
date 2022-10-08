84cc0fbb99ced1c0153449d96430b5b39f4c677e
Dianakyz <3222777my@gmail.com>
2022-10-08 12:35:36 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.println(Arrays.toString(incrementAndSlice(arr)));
     }
-}
+
+    public static int [] incrementAndSlice (int [] arr){
+        int[] slice = new int[5];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] += 1;
+            }
+            if (i >= 2 && i <= 6) {
+                slice[j] = arr[i];
+                j++;
+            }
+        }
+        return slice;
+    }
+    }
